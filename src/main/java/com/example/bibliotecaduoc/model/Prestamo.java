5c89b74e17d3e896f38a11c2beb8276e46bd30fa
Nicolás <nico.vallejos@duocuc.cl>
2025-04-29 13:24:31 -0400

    Prestamo final
    Con las modificaciones del profesor se cambio el modelo Prestamo

--- src/main/java/com/example/bibliotecaduoc/model/Prestamo.java
+++ src/main/java/com/example/bibliotecaduoc/model/Prestamo.java
@@ -5,7 +5,7 @@ package com.example.bibliotecaduoc.model;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Data;
-
+import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,8 +14,8 @@ public class Prestamo {
     private int idPrestamo;
     private int libroid;
     private String runSolicitante;
-    private int fechaSolicitud;
-    private int fechaEntrega;
+    private Date fechaSolicitud = new Date();
+    private Date fechaEntrega = new Date();
     private int cantidadDias;
     private int multas;
 }
